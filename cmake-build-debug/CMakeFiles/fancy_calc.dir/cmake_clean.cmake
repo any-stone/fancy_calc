b43c6e0af390cc8fcd979b1731dd4bc7dc841523
any-stone <kuxa.business@gmail.com>
2021-08-13 19:58:50 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/fancy_calc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fancy_calc.dir/main.cpp.o"
+  "fancy_calc"
+  "fancy_calc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fancy_calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
